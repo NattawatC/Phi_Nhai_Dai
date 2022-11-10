c6219fa206171fe46ad0fa171573aa277189e9d3
atip <au.atip@outlook.com>
2022-11-10 14:26:57 +0700

    New branch

--- app/src/main/java/com/example/phi_nhai_dai/MainActivity.java
+++ app/src/main/java/com/example/phi_nhai_dai/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.front_page);
+        setContentView(R.layout.card_description);
     }
 }
