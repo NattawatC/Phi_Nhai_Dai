a78f9fb2cc61a4d7a0e494add5d4b2220f7025c9
NattawatC <90686843+NattawatC@users.noreply.github.com>
2022-11-13 04:04:06 +0700

    Create new file to run new activities

---
+++ app/src/main/java/com/example/phi_nhai_dai/Discover.java
@@ -0,0 +1,95 @@
+//package com.example.phi_nhai_dai;
+//
+//import androidx.annotation.NonNull;
+//        import androidx.appcompat.app.ActionBarDrawerToggle;
+//        import androidx.appcompat.app.AppCompatActivity;
+//        import androidx.drawerlayout.widget.DrawerLayout;
+//        import android.os.Bundle;
+//        import android.view.MenuItem;
+//        import android.content.Intent;
+//
+//public class Discover extends AppCompatActivity {
+//
+//    public DrawerLayout drawerLayout;
+//    public ActionBarDrawerToggle actionBarDrawerToggle;
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.card_description_relative);
+//
+//        // drawer layout instance to toggle the menu icon to open
+//        // drawer and back button to close drawer
+//        drawerLayout = findViewById(R.id.my_drawer_layout);
+//        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
+//
+//        // pass the Open and Close toggle for the drawer layout listener
+//        // to toggle the button
+//        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+//        actionBarDrawerToggle.syncState();
+//
+//        // to make the Navigation drawer icon always appear on the action bar
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//    }
+//
+//    // override the onOptionsItemSelected()
+//    // function to implement
+//    // the item click listener callback
+//    // to open and close the navigation
+//    // drawer when the icon is clicked
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//
+//        int id = item.getItemId();
+//
+//        if (id == R.id.nav_aboutus){
+//            Intent myIntent = new Intent(Discover.this, Favorite.class);
+//            startActivity(myIntent);
+//            return true;
+//        }
+//
+//        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+//            return true;
+//        }
+//        return super.onOptionsItemSelected(item);
+//    }
+//}
+package com.example.phi_nhai_dai;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toolbar;
+
+public class Discover extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.card_description_relative);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.navigation_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.nav_aboutus){
+            Intent myIntent = new Intent(Discover.this, Favorite.class);
+            startActivity(myIntent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
