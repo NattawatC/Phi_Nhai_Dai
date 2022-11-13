252a0aa9577cffe8fed15ef4d70f472a3cdad131
NattawatC <90686843+NattawatC@users.noreply.github.com>
2022-11-13 13:00:28 +0700

    Create navbar

--- app/src/main/java/com/example/phi_nhai_dai/Favorite.java
+++ app/src/main/java/com/example/phi_nhai_dai/Favorite.java
@@ -2,13 +2,48 @@ package com.example.phi_nhai_dai;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Favorite extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.card_description);
+        setContentView(R.layout.activity_favorite);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.navigation_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.nav_aboutus){
+            Intent myIntent = new Intent(Favorite.this, MainActivity.class);
+            startActivity(myIntent);
+            return true;
+        }
+
+        else if (id == R.id.nav_discover){
+            Intent myIntent = new Intent(Favorite.this, Discover.class);
+            startActivity(myIntent);
+            return true;
+        }
+
+        else if (id == R.id.nav_favourite){
+            Intent myIntent = new Intent(Favorite.this, Favorite.class);
+            startActivity(myIntent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
