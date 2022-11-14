7fce1f729599ae208214ee4290fa5d16b73f99d9
atip <au.atip@outlook.com>
2022-11-14 17:53:25 +0700

    Connect database

--- app/src/main/java/com/example/phi_nhai_dai/main_page/Database.java
+++ app/src/main/java/com/example/phi_nhai_dai/main_page/Database.java
@@ -1,33 +1,61 @@
 package com.example.phi_nhai_dai.main_page;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class Database extends SQLiteOpenHelper {
 
-    public static final String DB_PATH = "app/src/main/res/raw/data.db";
+    @SuppressLint("SdCardPath")
+    private static final String DB_PATH = "/data/data/com.example.phi_nhai_dai/databases/";
+    private static String DB_NAME = "place";
+    private SQLiteDatabase sqLiteDatabase;
+    private final Context context;
 
-    public static final String DB_NAME = "Data";
-
-
-    public Database(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, ".db", null, 1);
+    public Database(@Nullable Context context) {
+        super(context, DB_NAME, null, 1);
+        this.context = context;
     }
 
-    //To create a new Database
-    @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String CreateTableStatement = "CREATE TABLE PLACES_TABLE()";
+    public void copyDB() throws IOException{
+        try {
+            Log.i("inside copyDB....................","start");
 
-        sqLiteDatabase.execSQL(CreateTableStatement);
+            InputStream ip =  context.getAssets().open(DB_NAME+".db");
+            Log.i("Input Stream....",ip+"");
+            String op=  DB_PATH  +  DB_NAME ;
+            OutputStream output = new FileOutputStream( op);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = ip.read(buffer))>0){
+                output.write(buffer, 0, length);
+                Log.i("Content.... ",length+"");
+            }
+            output.flush();
+            output.close();
+            ip.close();
+        }
+        catch (IOException e) {
+            Log.v("error", e.toString());
+        }
     }
 
-    //Prevent previous users apps from breaking when you change the database design
-    @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
 
+    public void openDB() throws SQLException {
+
+        String myPath = DB_PATH + DB_NAME;
+        sqLiteDatabase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
+        Log.i("open DB......",sqLiteDatabase.toString());
     }
+
 }
---
+++ app/src/main/java/com/example/phi_nhai_dai/main_page/Place.java
@@ -0,0 +1,9 @@
+package com.example.phi_nhai_dai.main_page;
+
+public class Place {
+    public int id;
+    public String name;
+    public String location;
+
+    public
+}
