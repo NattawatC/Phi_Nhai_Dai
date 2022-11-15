e5ac066b2f24785723592168318c5ddf01e2e7f3
NattawatC <90686843+NattawatC@users.noreply.github.com>
2022-11-15 23:35:50 +0700

    Add button to Front_page

--- app/src/main/java/com/example/phi_nhai_dai/MainActivity.java
+++ app/src/main/java/com/example/phi_nhai_dai/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.phi_nhai_dai;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.card_description);
+        setContentView(R.layout.front_page);
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(v -> openHomePage());
     }
+
+    private void openHomePage() {
+        startActivity(new Intent(this, Discover.class));
+    }
+
 }
+
+
