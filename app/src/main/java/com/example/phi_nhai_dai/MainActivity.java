0a13346f60ceeb15514bc984d985eaac8bdafb6f
NattawatC <90686843+NattawatC@users.noreply.github.com>
2022-11-16 02:18:49 +0700

    Go to main_page

--- app/src/main/java/com/example/phi_nhai_dai/MainActivity.java
+++ app/src/main/java/com/example/phi_nhai_dai/MainActivity.java
@@ -5,18 +5,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.example.phi_nhai_dai.main_page.MainPage;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.front_page);
+//        getSupportActionBar().hide();
         Button loginButton = findViewById(R.id.login_button);
         loginButton.setOnClickListener(v -> openHomePage());
     }
 
     private void openHomePage() {
-        startActivity(new Intent(this, Discover.class));
+        startActivity(new Intent(this, MainPage.class));
     }
 
 }
