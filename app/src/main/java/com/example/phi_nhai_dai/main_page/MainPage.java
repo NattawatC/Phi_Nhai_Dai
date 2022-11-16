2a0b39678743ca17dd808280f720f9d063c26624
NattawatC <90686843+NattawatC@users.noreply.github.com>
2022-11-16 22:20:46 +0700

    Add Dropdown menu and Change its color

--- app/src/main/java/com/example/phi_nhai_dai/main_page/MainPage.java
+++ app/src/main/java/com/example/phi_nhai_dai/main_page/MainPage.java
@@ -1,19 +1,27 @@
 package com.example.phi_nhai_dai.main_page;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatCheckBox;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.core.widget.CompoundButtonCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.phi_nhai_dai.Fav.Favorite;
 import com.example.phi_nhai_dai.MainActivity;
@@ -26,14 +34,37 @@ import java.util.Collections;
 
 public class MainPage extends AppCompatActivity {
 
+//    New Add
+    // Category Selector Box
+    boolean categoryBoxState = true;
+    boolean categoryLoaded = false;
+    GridLayout categorySelectorBox;
+    TextView chooseCategoryTitle;
+    ImageButton categoryDropdownArrow;
+    ArrayList<TextView> categoryTextViews;
+    ArrayList<AppCompatCheckBox> categoryCheckBoxes;
+    TextView main_dish_text;
+    TextView appetizer_text;
+    TextView desserts_text;
+    TextView snacks_text;
+    TextView beverages_text;
+    AppCompatCheckBox main_dish_checkbox;
+    AppCompatCheckBox appetizer_checkbox;
+    AppCompatCheckBox desserts_checkbox;
+    AppCompatCheckBox snacks_checkbox;
+    AppCompatCheckBox beverages_checkbox;
+
+    // Main Grid Layout
+    GridLayout parentGridLayout;
+//    -------------
+
     @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.new_discover_page);
 //        getSupportActionBar().hide();
-
-
+        initializeInstances();
         Database db = new Database(this);
 
         try {
@@ -76,7 +107,113 @@ public class MainPage extends AppCompatActivity {
             location.add(PlaceArrayList.get(i).getLocation());
             img_link.add(PlaceArrayList.get(i).getImg_link());
         }
+//New Add
+        // Category Dropdown Settings
+        chooseCategoryTitle.setOnClickListener(this::toggleCategoryBox);
+        categoryDropdownArrow.setOnClickListener(this::toggleCategoryBox);
+//        ----------
     }
+
+//New Add
+    private int getValueInDp(int value) {
+        float scale = getResources().getDisplayMetrics().density;
+        return (int) (value * scale + 0.5f);
+    }
+//    ---------
+
+//    New Add
+
+    private void initializeInstances() {
+
+        // Initializing page elements and objects
+        parentGridLayout            = findViewById(R.id.main_grid_layout);
+        chooseCategoryTitle         = findViewById(R.id.choose_category_title);
+        categorySelectorBox         = findViewById(R.id.category_selector_box);
+        categoryDropdownArrow       = findViewById(R.id.category_arrow);
+
+        // Category Dropdown Box Elements
+        categoryTextViews   = new ArrayList<>();
+        main_dish_text      = new TextView(this);
+        appetizer_text      = new TextView(this);
+        desserts_text       = new TextView(this);
+        snacks_text         = new TextView(this);
+        beverages_text      = new TextView(this);
+
+        categoryCheckBoxes  = new ArrayList<>();
+        main_dish_checkbox  = new AppCompatCheckBox(this);
+        appetizer_checkbox  = new AppCompatCheckBox(this);
+        desserts_checkbox   = new AppCompatCheckBox(this);
+        snacks_checkbox     = new AppCompatCheckBox(this);
+        beverages_checkbox  = new AppCompatCheckBox(this);
+    }
+    //    -------------
+
+//    New Add
+    // Expand and minimize category selector box
+    public void toggleCategoryBox(View view) {
+        if (!categoryLoaded) {
+            // Loads TextViews into ArrayList
+            categoryTextViews.add(main_dish_text);
+            categoryTextViews.add(appetizer_text);
+            categoryTextViews.add(desserts_text);
+            categoryTextViews.add(snacks_text);
+            categoryTextViews.add(beverages_text);
+
+            // Loads Checkboxes to ArrayList
+            categoryCheckBoxes.add(main_dish_checkbox);
+            categoryCheckBoxes.add(appetizer_checkbox);
+            categoryCheckBoxes.add(desserts_checkbox);
+            categoryCheckBoxes.add(snacks_checkbox);
+            categoryCheckBoxes.add(beverages_checkbox);
+
+            // Category UI Elements Setup
+            main_dish_text.setText(R.string.main_dish);
+            appetizer_text.setText(R.string.appetizers);
+            desserts_text.setText(R.string.desserts);
+            snacks_text.setText(R.string.snacks);
+            beverages_text.setText(R.string.beverages);
+
+            Typeface poppins_bold = ResourcesCompat.getFont(this, R.font.poppins_bold);
+
+            for (TextView t : categoryTextViews) {
+                t.setTextSize(18);
+                t.setTextColor(getResources().getColor(R.color.secondary));
+                t.setPadding(getValueInDp(20), 0, getValueInDp(200), 0);
+                t.setTypeface(poppins_bold);
+            }
+
+            for (AppCompatCheckBox c : categoryCheckBoxes) {
+                c.setGravity(Gravity.END);
+                c.setPadding(0, 0, 0, 0);
+                setCheckBoxColor(c, 0xFFF3F7F0, 0xFFF3F7F0);
+            }
+
+            categoryTextViews.get(categoryTextViews.size()-1)
+                    .setPadding(getValueInDp(20), 0, 0, getValueInDp(8));
+            categoryCheckBoxes.get(categoryCheckBoxes.size()-1)
+                    .setPadding(0, 0, getValueInDp(5), getValueInDp(8));
+
+            categoryLoaded = true;
+        }
+
+        // Performing tasks based on CategoryBoxState boolean
+        if (categoryBoxState) {
+            categoryDropdownArrow.animate().rotation(90).setDuration(300);
+
+            for (int i = 0; i < categoryTextViews.size(); i++) {
+                categorySelectorBox.addView(categoryTextViews.get(i));
+                categorySelectorBox.addView(categoryCheckBoxes.get(i));
+            }
+        }
+        else {
+            categoryDropdownArrow.animate().rotation(0).setDuration(300);
+            for (int i = 0; i < categoryTextViews.size() * 2; i++)
+                categorySelectorBox.removeViewAt(2);
+        }
+        categoryBoxState = !categoryBoxState;
+    }
+//    -------------
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
@@ -108,6 +245,21 @@ public class MainPage extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+//    Change checkbox color
+    public static void setCheckBoxColor(AppCompatCheckBox checkBox, int uncheckedColor, int checkedColor) {
+        ColorStateList colorStateList = new ColorStateList(
+                new int[][] {
+                        new int[] { -android.R.attr.state_checked }, // unchecked
+                        new int[] {  android.R.attr.state_checked }  // checked
+                },
+                new int[] {
+                        uncheckedColor,
+                        checkedColor
+                }
+        );
+        CompoundButtonCompat.setButtonTintList(checkBox, colorStateList);
     }
 
 }
