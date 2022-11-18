85723e9bd9e3425ef0cb96f0656c85341c25f598
atip <au.atip@outlook.com>
2022-11-19 04:01:39 +0700

    Edit Description.java

---
+++ app/src/main/java/com/example/phi_nhai_dai/Description/Description.java
@@ -0,0 +1,70 @@
+package com.example.phi_nhai_dai.Description;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import com.example.phi_nhai_dai.R;
+import com.example.phi_nhai_dai.main_page.Database;
+import com.example.phi_nhai_dai.main_page.MainPage;
+import com.example.phi_nhai_dai.main_page.Place;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Objects;
+
+
+public class Description extends AppCompatActivity {
+
+    TextView name;
+    TextView location;
+    ImageView image;
+
+    SQLiteDatabase db1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        setContentView(R.layout.card_description_relative);
+        db1 = openOrCreateDatabase("place", Context.MODE_PRIVATE, null);
+        loadActivity();
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void loadActivity() {
+
+        String id = getIntent().getStringExtra("ID");
+
+        Place p;
+        Cursor c = db1.rawQuery("SELECT * FROM Places "
+                + "WHERE ID=" + id, null);
+        c.moveToFirst();
+        p = new Place(c.getInt(0), c.getString(1)
+                , c.getString(2), c.getString(3), c.getFloat(4), c.getString(5));
+
+        name = findViewById(R.id.title_example);
+        name.setText(p.getName());
+
+        location = findViewById(R.id.location_example);
+        location.setText(p.getLocation() +", Thailand");
+
+        image = findViewById(R.id.cream_box);
+        Glide.with(this).load(p.getImg_link()).into(image);
+
+
+
+    }
+
+
+}
